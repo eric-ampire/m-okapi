2ecaeab97e3d73b69838ec2833dbd18250c72929
Dan kabulo <16km081@esisalama.org>
2019-01-01 23:17:30 +0100

    affichage du solde

--- application/views/utilisateur/accueil.php
+++ application/views/utilisateur/accueil.php
@@ -12,5 +12,23 @@
 </p>
 <h3>Exercices budgetaire en cours</h3>
 <p>
-    votre solde :
+    votre solde : <h2>
+    <?php
+        $identifiant = $this->session->id;  
+        $db = new PDO('mysql:host=localhost; dbname=mokapi', 'root', '');
+        $str = 'SELECT budget_initial FROM exercice_budgetaire WHERE id_utilisateur = :id_utilisateur';
+        $req = $db->prepare($str);
+        $val = array(
+            'id_utilisateur' => $identifiant
+        );
+        $req->execute($val);
+        
+        $tab_sujet = array();
+        while($s = $req->fetch(PDO::FETCH_OBJ))
+        {
+            echo $s->budget_initial;
+        }
+        
+       
+    ?></h2>
 </p>
