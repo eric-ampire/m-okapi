cf351858fdb80d3a6e5c2decec7f298b398492c0
Dan kabulo <16km081@esisalama.org>
2019-01-03 13:42:02 +0100

    arrangement

--- application/views/utilisateur/accueil.php
+++ application/views/utilisateur/accueil.php
@@ -80,6 +80,90 @@ body {
         echo $solde."$";
         echo "</h2></br>";
         echo "Entrees : <h2>".$f_entree."$</h2>";
+        echo "</br>
+</p>
+<p>
+    <h3>Progression evolution :</h3>";
+
+    $identifiant = $this->session->id;  
+    $db = new PDO('mysql:host=localhost; dbname=mokapi', 'root', '');
+    $str = 'SELECT montant_utilise FROM 
+    action_budgetaire';
+    $req = $db->prepare($str);
+    $val = array(
+        'id_utilisateur' => $identifiant
+    );
+    $req->execute($val);
+    
+    $tab_sujet = array();
+    $montant_utilise = 0;
+    while($s = $req->fetch(PDO::FETCH_OBJ))
+    {
+        $montant_utilise= $s->montant_utilise + $montant_utilise;
+    }
+    $montant = $montant_utilise;
+    echo "<h2>".$montant."$</h2> utilise   soit :<h2>".(($montant/$solde)*100)."%</h2> consomme's "; 
+    
+    ?>
+</p>
+
+<p>
+    <h3>Calendrier de l'evolution :</h3> 
+    <?php  
+        echo $this->calendar->generate();
+        echo "</br>";
+        $identifiant = $this->session->id;  
+        $db = new PDO('mysql:host=localhost; dbname=mokapi', 'root', '');
+        $str = 'SELECT date_creation, date_debut, date_fin FROM 
+        exercice_budgetaire WHERE id_utilisateur = :id_utilisateur';
+        $req = $db->prepare($str);
+        $val = array(
+            'id_utilisateur' => $identifiant
+        );
+        $req->execute($val);
+        
+        $tab_sujet = array();
+        $solde = 0;
+        while($s = $req->fetch(PDO::FETCH_OBJ))
+        {
+            $dt_creation = date_create($s->date_creation);
+            $dt_debut = date_create($s->date_debut);
+            $dt_fin = date_create($s->date_fin);
+        }
+
+        echo "Ce budget a ete initialise en date du :".date_format($dt_creation,'d-m-Y');
+        echo "</br>Sa duree va du :".date_format($dt_debut,'d-m-Y');
+        echo"</br>Au :".$fin = date_format($dt_fin,'d-m-Y');
+        
+        $today = date('d-m-Y');
+        $result = $fin - $today;
+        echo "</br> Jours restants :<h3>".$result." jour(s)</h3";
+    
+    
+    ?>
+</p>
+
+<p>
+    <h3>Les derniers mouvements</h3> 
+    <?php
+        $identifiant = $this->session->id;  
+        $db = new PDO('mysql:host=localhost; dbname=mokapi', 'root', '');
+        $str = 'SELECT montant_utilise, motif, date_creation FROM 
+        action_budgetaire';
+        $req = $db->prepare($str);
+        $val = array(
+            'id_utilisateur' => $identifiant
+        );
+        $req->execute($val);
+        
+        $tab_sujet = array();
+        $solde = 0;
+        while($s = $req->fetch(PDO::FETCH_OBJ))
+        {
+            echo "Montant utilise :".$s->montant_utilise." motif :".
+            $s->motif." date de creation :".$s->date_creation."</br>";
+        }
+
     ?>
 </p>
 
