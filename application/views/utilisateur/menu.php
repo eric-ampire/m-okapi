d46760de21a4343d352b5e69c02790c55057cb00
Dan kabulo <16km081@esisalama.org>
2019-01-03 11:27:47 +0100

    redirection du lie accueil

--- application/views/utilisateur/menu.php
+++ application/views/utilisateur/menu.php
@@ -1,6 +1,6 @@
 <h1>M-OKAPI</h1>
 <p>
-    <a href="">Accueil</a></br>
+    <a href="<?php echo site_url('utilisateur/accueil'); ?>">Accueil</a></br>
     <a href="">Evolution/ Rappoort</a></br>
     <a href="<?php echo site_url('utilisateur/exercices_budgetaire')  ?>">Nouvel exercices budgetaire</a></br>
     <a href="<?php echo site_url('utilisateur/parametre'); ?>">Parametre</a></br>
