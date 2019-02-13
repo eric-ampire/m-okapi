80f24cd060a65efb7d30d04ed2408efe23044a2f
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 00:49:32 +0200

    Adding Sortie Entity

---
+++ application/models/entity/Sortie.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDM
+ * Date: 14/02/2019
+ * Time: 00:48
+ */
+
+class Sortie
+{
+    private $id;
+    private $idCategorieSortie;
+    private $idExercie;
+}
