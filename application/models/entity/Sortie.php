2bdcce843932efe7138ff7be19237f2f1b0a6b47
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 00:50:22 +0200

    Refactoring Sortie Entity

--- application/models/entity/Sortie.php
+++ application/models/entity/Sortie.php
@@ -11,4 +11,67 @@ class Sortie
     private $id;
     private $idCategorieSortie;
     private $idExercie;
+
+    /**
+     * Sortie constructor.
+     * @param $id
+     * @param $idCategorieSortie
+     * @param $idExercie
+     */
+    public function __construct($id, $idCategorieSortie, $idExercie)
+    {
+        $this->id = $id;
+        $this->idCategorieSortie = $idCategorieSortie;
+        $this->idExercie = $idExercie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdCategorieSortie()
+    {
+        return $this->idCategorieSortie;
+    }
+
+    /**
+     * @param mixed $idCategorieSortie
+     */
+    public function setIdCategorieSortie($idCategorieSortie)
+    {
+        $this->idCategorieSortie = $idCategorieSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdExercie()
+    {
+        return $this->idExercie;
+    }
+
+    /**
+     * @param mixed $idExercie
+     */
+    public function setIdExercie($idExercie)
+    {
+        $this->idExercie = $idExercie;
+    }
+
+
 }
