d3863b4952f28baf2b1ccb788f9931e8cbdc9f0c
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 01:11:42 +0200

    Adding SortieModel

---
+++ application/models/model/SortieModel.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDM
+ * Date: 14/02/2019
+ * Time: 01:06
+ */
+
+class SortieModel extends CI_Model
+{
+    private $tableName= 'sortie';
+
+    function add(Sortie $sortie) {
+        $sortie->getId();
+        $sortie->getIdCategorieSortie();
+        $sortie->getIdExerciceBudgetaire();
+        $sortie->getSeuil();
+
+        $data = array(
+            "id" => $sortie->getId(),
+            'id_categorie_sortie'=> $sortie->getIdCategorieSortie(),
+            'id_exercice_budgetaire' => $sortie->getIdExerciceBudgetaire(),
+            'seuil' => $sortie->getSeuil()
+        );
+
+        $this->db->insert($this->tableName, $data);
+    }
+
+    function getOne($id) {
+        $this->db->select("*");
+        $this->db->form($this->tableName);
+        $this->where("id", $id);
+
+        return $this->db->get()->result();
+    }
+
+    function getAll() {
+        $this->db->select("*");
+        $this->db->form($this->tableName);
+
+        return $this->db->get()->result();
+    }
+}
