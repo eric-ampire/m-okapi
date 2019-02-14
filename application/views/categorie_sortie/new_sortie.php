56565849ca20a8e74824ba42c0df8799d8550463
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 08:54:11 +0200

    Le champ doit etre required

--- application/views/categorie_sortie/new_sortie.php
+++ application/views/categorie_sortie/new_sortie.php
@@ -34,7 +34,7 @@ body {
 
 <form method="post" action="<?php echo site_url('utilisateur/nouvelle_categorie_sortie') ?>" >
     Nom categorie :
-	<input type="text" name="nom" /></br>
+	<input type="text" name="nom" required/></br>
     <input type="submit" valuue="Enregistrer" /></br>
 </form>
 <a href="<?php  echo site_url('utilisateur/cont_parametre'); ?>">Retour</a>
