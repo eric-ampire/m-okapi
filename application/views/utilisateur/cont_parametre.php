61511c8eda2a737a625bf1bee830cc389b3e0d18
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 08:47:09 +0200

    Update cont_parametre.php

--- application/views/utilisateur/cont_parametre.php
+++ application/views/utilisateur/cont_parametre.php
@@ -32,7 +32,7 @@ body {
 }</style>
 <h3>Parametre budgetaire</h3>
 <p>
-    <a href="<?php echo site_url('utilisateur/categorie_entree'); ?>">Nouvelle categorie d'entree</a></br>
+    <a href="<?php echo site_url('utilisateur/entree'); ?>">Nouvelle entree</a></br>
     <a href="<?php echo site_url('utilisateur/categorie_sortie'); ?>">Nouvelle categorie de sortie</a></br>
     <a href="<?php echo site_url('utilisateur/action_budgetaire') ?>">Nouvelle action</a></br>
     <a href="<?php echo site_url('utilisateur/sortie'); ?>">Nouvelle sortie</a></br>
