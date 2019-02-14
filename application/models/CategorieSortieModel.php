bf5a17e301176df46266c2ea352932d1fac0ca0b
Eric Ampire <ericampire.top@gmail.com>
2019-02-14 08:39:03 +0200

    Update CategorieSortieModel

--- application/models/CategorieSortieModel.php
+++ application/models/CategorieSortieModel.php
@@ -1,8 +1,29 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: EDM
+ * Date: 14/02/2019
+ * Time: 01:47
+ */
 
 class CategorieSortieModel extends CI_Model
 {
+    private $tableName = 'categorie_sortie';
 
-    
+    public function add($data) {
+        $this->db->insert($this->tableName, $data);
+    }
+
+    function getByUser($idUser) {
+        $this->db->select('*');
+        $this->db->from($this->tableName);
+        $this->db->where("id_utilisateur", $idUser);
+
+        $result = $this->db->get()->result();
+        if (count($result) > 0) {
+            return $result;
+        } else {
+            return array();
+        }
+    }
 }
